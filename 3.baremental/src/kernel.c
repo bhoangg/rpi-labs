f5e19092bef6337aa3f359b425337dbed8d6cb3c
Binh Hoang <binh.hoangtuan.hust@gmail.com>
2021-04-21 23:06:53 +0700

    add baremental

---
+++ 3.baremental/src/kernel.c
@@ -0,0 +1,22 @@
+#define GPIO_BASE_ADDR  (0x3F200000)
+#define GPFSEL1         *((unsigned int *)(GPIO_BASE_ADDR+0x04))
+#define GPSET0          *((unsigned int *)(GPIO_BASE_ADDR+0x1C))
+#define GPCLR0          *((unsigned int *)(GPIO_BASE_ADDR+0x28))
+
+void delays(int times);
+void kernel_main(void)
+{
+	//set gpio17 is output
+	GPFSEL1=(GPFSEL1&(~(7<<21)))|(1<<21);
+	while (1) {
+		GPSET0=GPSET0|(1<<17);
+		delays(1000000);
+		GPCLR0=GPCLR0|(1<<17);
+		delays(1000000);
+	}
+}
+
+void delays(int times){
+	int count = 0;
+	while(count++ < times);
+}
